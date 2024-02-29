a6f54896631e4b3f5e6ac726d464957a9143de48
Maxim Klassen <maximklassen@mail.de>
2024-02-29 15:15:19 +0100

    Added auto rotate for cropped images

--- src/Aux/Image.php
+++ src/Aux/Image.php
@@ -94,6 +94,51 @@ class Image
         $crateFunction = 'imagecreatefrom' . $extension;
         $saveFunction = 'image' . $extension;
         $image = $crateFunction($filePath);
+        if ($extension == 'jpeg') {
+            $exif = exif_read_data($filePath);
+            if ($image && $exif && isset($exif['Orientation'])) {
+                $ort = $exif['Orientation'];
+
+                if ($ort == 6 || $ort == 5) {
+                    $image = imagerotate($image, 270, 0);
+                    $tw = $tmpWidth;
+                    $iw = $imageWidth;
+                    $w = $width;
+                    $tmpWidth = $tmpHeight;
+                    $imageWidth = $imageHeight;
+                    $width = $height;
+                    $tmpHeight = $tw;
+                    $imageHeight = $iw;
+                    $height = $w;
+                    $x = $dstX;
+                    $dstX = $dstY;
+                    $dstY = $x;
+                }
+                if ($ort == 3 || $ort == 4) {
+                    $image = imagerotate($image, 180, 0);
+                }
+                if ($ort == 8 || $ort == 7) {
+                    $image = imagerotate($image, 90, 0);
+                    $tw = $tmpWidth;
+                    $iw = $imageWidth;
+                    $w = $width;
+                    $tmpWidth = $tmpHeight;
+                    $imageWidth = $imageHeight;
+                    $width = $height;
+                    $tmpHeight = $tw;
+                    $imageHeight = $iw;
+                    $height = $w;
+                    $x = $dstX;
+                    $dstX = $dstY;
+                    $dstY = $x;
+                }
+
+                if ($ort == 5 || $ort == 4 || $ort == 7) {
+                    imageflip($image, IMG_FLIP_HORIZONTAL);
+                }
+            }
+        }
+
         $tmpImage = imagecreatetruecolor($tmpWidth, $tmpHeight);
         if ($imageData[2] == 1 || $imageData[2] == 3 || $imageData[2] == 18) {
             imagealphablending($tmpImage, true);
--- src/View/Composers/EmailEventComposer.php
+++ src/View/Composers/EmailEventComposer.php
@@ -34,16 +34,16 @@ class EmailEventComposer
     public function compose(View $view)
     {
         $view->with('elfUserData',[
-            'userEmail' => $view->emailEvent->eventUser->email ?? null,
-            'userName' => $view->emailEvent->eventUser->name() ?? null,
-            'userFirstName' => $view->emailEvent->eventUser->data->first_name ?? null,
-            'userSecondName' => $view->emailEvent->eventUser->data->second_name ?? null,
-            'userLastName' => $view->emailEvent->eventUser->data->last_name ?? null,
-            'userFormOfAddress' => $view->emailEvent->eventUser->data->form_of_address->form ?? null,
-            'userFormOfAddressLang' => !empty($view->emailEvent->eventUser->data->form_of_address) ? __('elfcms::default.'.$view->emailEvent->eventUser->data->form_of_address->lang_string) : null,
-            'userGenderForm' => $view->emailEvent->eventUser->data->gender->name ?? null,
-            'userGenderFormCode' => $view->emailEvent->eventUser->data->gender->code ?? null,
-            'userGenderFormLang' => !empty($view->emailEvent->eventUser->data->gender) ? __('elfcms::default.'.$view->emailEvent->eventUser->data->gender->lang_string) : null
+            'userEmail' => $view->emailEvent->eventUser?->email ?? null,
+            'userName' => $view->emailEvent->eventUser?->name() ?? null,
+            'userFirstName' => $view->emailEvent->eventUser?->data->first_name ?? null,
+            'userSecondName' => $view->emailEvent->eventUser?->data->second_name ?? null,
+            'userLastName' => $view->emailEvent->eventUser?->data->last_name ?? null,
+            'userFormOfAddress' => $view->emailEvent->eventUser?->data->form_of_address->form ?? null,
+            'userFormOfAddressLang' => !empty($view->emailEvent?->eventUser->data->form_of_address) ? __('elfcms::default.'.$view->emailEvent->eventUser->data->form_of_address->lang_string) : null,
+            'userGenderForm' => $view->emailEvent->eventUser?->data->gender->name ?? null,
+            'userGenderFormCode' => $view->emailEvent->eventUser?->data->gender->code ?? null,
+            'userGenderFormLang' => !empty($view->emailEvent?->eventUser->data->gender) ? __('elfcms::default.'.$view->emailEvent->eventUser->data->gender->lang_string) : null
         ]);
     }
 }
