da1fa88e6f455611d218075d1b81bee399c29461
Maxim Klassen <maximklassen@mail.de>
2024-03-04 15:09:54 +0100

    maintenance mode is only off for admin user

--- src/Http/Middleware/Maintenance.php
+++ src/Http/Middleware/Maintenance.php
@@ -25,6 +25,41 @@ class Maintenance
     {
         $isMaintenance = boolval(Setting::value('site_maintenance')) ?? false;
         if ($isMaintenance && !Str::startsWith(Route::current()->getName(), 'admin')) {
+
+            if (Auth::check()) {
+                $user = Auth::user();
+                if ($user) {
+                    $currentRoute = Route::currentRouteName();
+                    $roleIDs = [];
+                    foreach ($user->roles as $role) {
+                        $roleIDs[] = $role->id;
+                        if ($role->code == 'admin') {
+                            return $next($request);
+                        }
+                    }
+                    $accessRoutes = Permissions::routes(false);
+                    $read = 0;
+                    $write = 0;
+                    if ($currentRoute == 'admin.index') {
+                        $perms = RolePermission::whereIn('role_id',$roleIDs)->get();
+                        if ($perms->sum('read') > 0 || $perms->sum('write') > 0) {
+                            return $next($request);
+                        }
+                    }
+                    foreach ($accessRoutes as $routeName => $routeData) {
+                        if (Str::startsWith($currentRoute, $routeName)) {
+                            $permissions = RolePermission::where('route',$routeName)->whereIn('role_id',$roleIDs)->get();
+                            $read += $permissions->sum('read');
+                            $write += $permissions->sum('write');
+                        }
+                    }
+                    if ($read > 0) {
+                        return $next($request);
+                    }
+
+                }
+            }
+
             $text = Setting::value('site_maintenance_text') ?? __('elfcms::default.site_under_construction');
             return response(view('maintenance',[
                 'page' => [
